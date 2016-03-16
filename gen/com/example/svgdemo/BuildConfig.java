5a06d431152db5c36066197fd59062aadd2e8c62
Your Name <you@example.com>
2016-03-16 15:28:41 +0800

    first commit

---
+++ gen/com/example/svgdemo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.svgdemo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/svgdemo/SecondActivity.java
@@ -0,0 +1,48 @@
+package com.example.svgdemo;
+
+import com.eftimoff.androipathview.PathView;
+
+import android.app.Activity;
+import android.graphics.Path;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+
+public class SecondActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_second);
+		final PathView pathView = (PathView) findViewById(R.id.pathView);
+		// final Path path = makeConvexArrow(50, 100);
+		// pathView.setPath(path);
+		// pathView.setFillAfter(true);
+		// pathView.useNaturalColors();
+		pathView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				pathView.getPathAnimator()
+						.
+						// pathView.getSequentialPathAnimator().
+						delay(100).duration(1500)
+						.interpolator(new AccelerateDecelerateInterpolator())
+						.start();
+			}
+		});
+	}
+
+	private Path makeConvexArrow(float length, float height) {
+		final Path path = new Path();
+		path.moveTo(0.0f, 0.0f);
+		path.lineTo(length / 4f, 0.0f);
+		path.lineTo(length, height / 2.0f);
+		path.lineTo(length / 4f, height);
+		path.lineTo(0.0f, height);
+		path.lineTo(length * 3f / 4f, height / 2f);
+		path.lineTo(0.0f, 0.0f);
+		path.close();
+		return path;
+	}
+
+}
